0d9ba7d91efa82c0fbf90418bb724ef9f8556d37
teeshaghevariya <tishaghevariya@gmail.com>
2025-08-12 15:00:26 +0530

    IMPORTANT: Deleted all the tests.

--- e2e/helpers/pages/admin/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './analytics/AnalyticsGrowthPage';
-export * from './analytics/AnalyticsLocationsPage';
-export * from './analytics/AnalyticsOverviewPage';
-export * from './analytics/AnalyticsWebTrafficPage';
-export * from './post-analytics/PostAnalyticsPage';
